12e82b75e1611cd198670ba05af6c67f0d2714c5
Ikko Fukuda <fukuda@garoo.co.jp>
2016-01-20 14:55:48 +0900

    test

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -12,7 +12,7 @@ return array(
       //'dsn' => 'sqlsrv:server = tcp:j1ot5wy793.database.windows.net,1433; Database = infic_db',
       //sqlsrv:server = tcp:infic-test.database.windows.net,1433; Database = infic_test
       'dsn' => 'sqlsrv:server = tcp:infic-test.database.windows.net,1433; Database = infic_test',
-      'username' => 'infic',
+      'username' => 'infic-test',
       'password' => '2scHOVO6',
       'persistent' => false,
     ),
