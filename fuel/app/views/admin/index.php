09962c24a2af369f9a0c2b99703a022f2f0b14d8
Ikko Fukuda <fukuda@garoo.co.jp>
2016-01-26 15:46:32 +0900

    2016-01-26

--- fuel/app/views/admin/index.php
+++ fuel/app/views/admin/index.php
@@ -1,7 +1,46 @@
 <div class="row">
-	<div class="col-sm-4">
+	<div class="col-sm-3">
 		<div class="panel panel-default">
+			<div class="panel-body">
+				<p></p>
+			</div>
 			<div class="panel-heading">管理者一覧</div>
+			<ul class="list-group">
+			<?php
+			foreach($admins as $admin) {
+			?>
+				<li class="list-group-item"><?php echo $admin['username']; ?>
+			<?php
+			}
+			?>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="col-sm-6">
+		<form class="form-horizontal" id="login">
+		  <div class="form-group">
+		    <label for="username" class="col-sm-2 control-label">ID</label>
+		    <div class="col-sm-10">
+		      <input type="text" class="form-control" id="username" placeholder="User Name">
+		    </div>
+		  </div>
+		  <div class="form-group">
+		    <label for="password" class="col-sm-2 control-label">パスワード</label>
+		    <div class="col-sm-10">
+		      <input type="password" class="form-control" id="password" placeholder="Password">
+		    </div>
+		  </div>
+		  <div class="form-group">
+		    <div class="col-sm-offset-2 col-sm-10">
+		      <button type="submit" class="btn btn-default">ログインする</button>
+		    </div>
+		  </div>
+		</form>
+	</div>
+	<div class="col-sm-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">担当一覧</div>
 			<ul class="list-group">
 			<?php
 			foreach($admins as $admin) {
