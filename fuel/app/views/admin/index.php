dafc34a6af9262154ce0737185e7144169c7dce6
Ikko Fukuda <fukuda@garoo.co.jp>
2016-01-26 15:41:59 +0900

    2016-01-26

--- fuel/app/views/admin/index.php
+++ fuel/app/views/admin/index.php
@@ -1,10 +1,15 @@
-<ul>
-<?php
-foreach($admins as $admin) {
-?>
-	<li><?php echo $admin['username']; ?>
-<?php
-}
-?>
-	</li>
-</ul>
+<div class="row">
+	<div class="panel panel-default col-sm-4">
+		<div class="panel-heading">管理者一覧</div>
+		<ul class="list-group">
+		<?php
+		foreach($admins as $admin) {
+		?>
+			<li class="list-group-item"><?php echo $admin['username']; ?>
+		<?php
+		}
+		?>
+			</li>
+		</ul>
+	</div>
+</div>
