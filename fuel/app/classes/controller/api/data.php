44d88b535010acd24bf04af1bdcbca975a87fd8b
Ikko Fukuda <fukuda@garoo.co.jp>
2016-02-03 14:17:14 +0900

    test

--- fuel/app/classes/controller/api/data.php
+++ fuel/app/classes/controller/api/data.php
@@ -13,7 +13,7 @@ class Controller_Api_Data extends Controller_Api
 
 	public function get_test() {
 		$sql = "SELECT * FROM data";
-		$res = DB::query($sql)->execute();
+		$res = DB::query($sql)->execute("data");
 		$data = $res->as_array();
 		$this->result = $data;
  		return $this->result();
