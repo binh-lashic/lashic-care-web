a953751846a0219479a1fc638658814b0aa7d56e
Ikko Fukuda <fukuda@garoo.co.jp>
2016-02-09 13:34:22 +0900

    test

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -2,7 +2,7 @@
 <html lang="ja_JP">
 <head>
 	<meta charset="utf-8">
-	<title>Infic - <?php echo $title; ?></title>
+	<title>Infic - <?php echo isset($title) $title : ""; ?></title>
 	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -11,13 +11,22 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
-	<header class="navbar navbar-static-top" id="top" role="banner">
-		<div class=container>
-  			<div class=navbar-header>
-  				<a href=../ class=navbar-brand>Bootstrap</a>
-  			</div>
-		</div>
-	</header>  
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/admin/user">担当者</a></li>
+        <li><a href="/admin/sensor">機器</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 
 	<div class="container">
 		<?php echo $content; ?>
