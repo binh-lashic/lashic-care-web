43e59dfdd41fe8ca09e2a0f9b3aa6afa92031703
Ikko Fukuda <fukuda@garoo.co.jp>
2016-02-09 15:49:23 +0900

    20160209

--- fuel/app/config/test/db.php
+++ fuel/app/config/test/db.php
@@ -1,16 +1,33 @@
 <?php
 /**
- * The test database settings. These get merged with the global settings.
+ * Use this file to override global defaults.
  *
- * This environment is primarily used by unit tests, to run on a controlled environment.
+ * See the individual environment DB configs for specific config information.
  */
 
 return array(
-	'default' => array(
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_test',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
-		),
-	),
+  'default' => array(
+    'type' => 'pdo', // PDO を利用
+    'connection' => array(
+      'dsn' => 'sqlsrv:server = tcp:infic-test.database.windows.net,1433; Database = infic_test',
+      'username' => 'infic-test',
+      'password' => '2scHOVO6',
+      'persistent' => false,
+    ),
+    'identifier' => '',
+    'table_prefix' => '',
+    'charset' => NULL,
+  ),
+  'data' => array(
+    'type' => 'pdo', // PDO を利用
+    'connection' => array(
+      'dsn' => 'sqlsrv:server = tcp:j1ot5wy793.database.windows.net,1433; Database = infic_db',
+      'username' => 'qyNFJoQegELogin_infic-api',
+      'password' => 'Zm87el320546zZ$$',
+      'persistent' => false,
+    ),
+    'identifier' => '',
+    'table_prefix' => '',
+    'charset' => NULL,
+  ),
 );
