68cd19c9e692d620f29d1afb63e3268838f0044c
Ikko Fukuda <fukuda@garoo.co.jp>
2016-02-12 18:04:08 +0900

    DBのSQL Server対応

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -2,12 +2,6 @@
 // Bootstrap the framework DO NOT edit this
 require COREPATH.'bootstrap.php';
 
-\Autoloader::add_classes(array(
-	// Add classes you want to override here
-	// Example: 'View' => APPPATH.'classes/view.php',
-    'Database_Query_Builder_Select' => APPPATH.'classes/database/query/builder/select.php',
-));
-
 // Register the autoloader
 \Autoloader::register();
 
@@ -22,6 +16,11 @@ require COREPATH.'bootstrap.php';
 if(preg_match("/infic.garoo.jp/i", $_SERVER['HTTP_HOST'])) {
 	\Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : \Fuel::DEVELOPMENT);
 } else {
+	\Autoloader::add_classes(array(
+		// Add classes you want to override here
+		// Example: 'View' => APPPATH.'classes/view.php',
+	    'Database_Query_Builder_Select' => APPPATH.'classes/database/query/builder/select.php',
+	));
 	\Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : \Fuel::TEST);
 }
 
