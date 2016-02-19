b692f9e279891933bb30e6a7763580b58911531c
Ikko Fukuda <fukuda@garoo.co.jp>
2016-02-20 00:48:23 +0900

    アラートマスター作成機能の追加

---
+++ fuel/app/classes/controller/admin/alert.php
@@ -0,0 +1,14 @@
+<?php
+class Controller_Admin_Alert extends Controller_Template
+{
+
+	public function action_create() {
+		try {
+			\Model_Alert::createTable();
+		} catch(Exception $e) {
+			print_r($e->getMessage());
+		}
+		echo "作成しました";
+		exit;
+	}
+}
--- fuel/app/views/admin/page/master.php
+++ fuel/app/views/admin/page/master.php
@@ -5,5 +5,6 @@
 		<li><a href="/admin/sensor/create">sensorsテーブル作成</a></li>
 		<li><a href="/admin/user/sensor/create">user_sensorsテーブル作成</a></li>
 		<li><a href="/admin/user/client/create">user_clientsテーブル作成</a></li>
+		<li><a href="/admin/alert/create">alertsテーブル作成</a></li>
 	</ul>
 </div>
