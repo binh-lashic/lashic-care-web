06e323721a2ecaa32d9ead8e88f9049613f0840f
Ikko Fukuda <fukuda@garoo.co.jp>
2016-03-02 18:45:15 +0900

    reportページの追加

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	'_root_'  => 'page/index',  // The default route
+	'_root_'  => 'user/login',  // The default route
 	'_404_'   => 'page/404',    // The main 404 route
 	'admin' => array('admin/page', 'name' => 'index'),
 );
