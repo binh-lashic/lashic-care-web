20eec2466450abb149ea72b1d63e866f6b7dec96
Ikko Fukuda <fukuda@garoo.co.jp>
2016-03-02 19:38:41 +0900

    reportページの追加

--- fuel/app/classes/controller/user.php
+++ fuel/app/classes/controller/user.php
@@ -26,12 +26,12 @@ class Controller_User extends Controller_Page
 	    $this->data = array(
 	    	'user' => $this->user,
 	    	'clients' => $this->clients,
-	    	'client' => $client,
 	    );
 
 	    $client = Session::get("client");
 	    if(empty($client) && isset($this->clients)) {
 	    	$client = $this->clients[0];
+	    	$this->data['client'] = $client;
 		    $this->data['admins'] = \Model_User::getAdmins($client['id']);
 	    }
 
