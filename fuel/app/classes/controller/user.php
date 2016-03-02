a9440502a060afe63d39531a08bb4ee040228cc3
Ikko Fukuda <fukuda@garoo.co.jp>
2016-03-02 18:41:40 +0900

    reportページの追加

--- fuel/app/classes/controller/user.php
+++ fuel/app/classes/controller/user.php
@@ -53,6 +53,7 @@ class Controller_User extends Controller_Page
 					'limit' => Config::get("report_list_count"),
 				);
 				$this->data['header_alerts'] = \Model_Alert::getAlerts($params);
+				print_r($this->data['header_alerts']);
 			}
 		}
 	}
