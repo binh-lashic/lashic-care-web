f0786334e369b27a837f5f1492d5171075c7a414
Ikko Fukuda <fukuda@garoo.co.jp>
2016-03-15 19:21:44 +0900

    data dbへのindexの追加

--- fuel/app/classes/controller/admin/page.php
+++ fuel/app/classes/controller/admin/page.php
@@ -32,9 +32,10 @@ class Controller_Admin_Page extends Controller_Admin
         }
 
         public function action_create_index() {
+                /*
                 $rows = DB::query("CREATE INDEX data_date ON data (date)")->execute("data");
                 print_r($rows);
-
+                */
                 $rows = DB::query("CREATE INDEX data_sensor_id ON data (sensor_id)")->execute("data");
                 print_r($rows);
 
