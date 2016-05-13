8028b342940d787ebf99b7346387ef1df6ef1245
Ikko Fukuda <fukuda@garoo.co.jp>
2016-05-13 11:27:24 +0900

    静的ページへのリンクを追加

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -4,5 +4,7 @@ return array(
 	'_404_'   => 'page/404',    // The main 404 route
 	'admin' => array('admin/page', 'name' => 'index'),
 	'contents/careeye' => 'page/careeye',
-	'contents/infic' => 'page/infic'
+	'contents/infic' => 'page/infic',
+	'contents/info' => 'page/news',
+	'contents/kiyaku' => 'page/term',
 );
