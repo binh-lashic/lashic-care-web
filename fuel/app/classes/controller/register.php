a2211bc2585a4a1b9ceee655c46ec1693a0d3aa7
Ikko Fukuda <fukuda@garoo.co.jp>
2016-05-13 13:03:07 +0900

    新規登録ページ

--- fuel/app/classes/controller/register.php
+++ fuel/app/classes/controller/register.php
@@ -7,7 +7,7 @@ class Controller_Register extends Controller_Base
 		$this->nologin_methods = array(
 	        'index',
 	    );
-	    $this->template = 'template_all';
+	    $this->template = 'template';
 
 	    parent::before();
 	}
@@ -18,6 +18,7 @@ class Controller_Register extends Controller_Base
         $this->data['prefectures'] = Config::get("prefectures");
 
 		$this->template->title = 'Care Eye 新規登録  >  アカウント情報　入力';
+        $this->template->header = View::forge('header', $this->data);
         $this->template->content = View::forge('register/form', $this->data);
 	}
 }
