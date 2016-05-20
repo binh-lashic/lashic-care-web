d509712bfef76fca64c06cbbea77b91ed2565b17
Ikko Fukuda <fukuda@garoo.co.jp>
2016-05-21 01:14:38 +0900

    ユーザAPIのセンサーデータの構成の修正

--- fuel/app/classes/model/user.php
+++ fuel/app/classes/model/user.php
@@ -393,7 +393,13 @@ class Model_User extends Orm\Model{
 				),
 			));
 			foreach($rows as $row) {
-				$sensors[] = $row->to_array();
+				$user_sensor = $row->to_array();
+				$sensor = $user_sensor['sensor'];
+				unset($user_sensor['sensor']);
+				unset($user_sensor['id']);
+				unset($user_sensor['user_id']);
+				unset($user_sensor['sensor_id']);
+				$sensors[] = array_merge($sensor, $user_sensor);
 			}
 		}
 		return $sensors;
