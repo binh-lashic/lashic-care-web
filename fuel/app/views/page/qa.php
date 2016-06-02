0022bfe68c4043554f0a8bb1fa15a475ec98294b
Takeshi Nishino <bamblogs@Takeshi-no-MacBook.local>
2016-06-02 10:13:22 +0900

    INFIC-74 フッターの修正

---
+++ fuel/app/views/page/qa.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>CareEye ケアアイ - Q &amp; A</title>
+<meta name="keywords" content="ケアアイ,ケア,アイ,見守り">
+<meta name="description" content="ケアアイのQ & Aページです。">
+<meta name="robots" content="all">
+<meta property="og:type" content="website" >
+<meta property="og:url" content="http://careeye.jp/" >
+<meta property="og:locale" content="ja_JP" >
+<meta property="og:image" content="/images/common/ogp.jpg" >
+<meta property="og:site_name" content="CareEye（ケアアイ）　Q & Aページです。" >
+<meta property="og:description" content="CareEye（ケアアイ）は高齢者の”自立”をささえ、”あんしん”を共有します。" >
+<meta name="viewport" content="target-densitydpi=device-dpi, width=1000px, minimum-scale=0.1, maximum-scale=1.0, user-scalable=yes">
+<link rel="shortcut icon" href="http://careeye.jp/favicon.ico" type="image/vnd.microsoft.icon">
+<link rel="icon" href="http://careeye.jp/favicon.ico" type="image/vnd.microsoft.icon">
+<link rel="apple-touch-icon" href="http://careeye.jp/apple-touch-icon-precomposed.png">
+<link href="reset.css" type="text/css" rel="stylesheet">
+<link href="common.css" type="text/css" rel="stylesheet">
+<link href="drawer.css" type="text/css" rel="stylesheet">
+<link href="style.css" type="text/css" rel="stylesheet">
+<link href="print.css" type="text/css" rel="stylesheet" media="print" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
+<body id="home" class="drawer drawer--right">
+<header class="drawer-navbar" role="banner">
+	<div class="drawer-container">
+		<div class="drawer-navbar-header"><a class="logo" href="http://careeye.jp/user"><img src="../images/common/logo.png" alt=""/></a></div>
+	</div>
+</header>
+
+<!-- content -->
+<main role="main" class="clearBoth">
+	<!-- pankuzu -->
+	<div id="pankuzu">
+		<div class="content">
+			<a href="http://careeye.jp/user/">ホーム</a>
+		&nbsp;&gt;&nbsp; Q &amp; A</div>
+	</div>
+	<!-- /pankuzu -->
+
+	<div class="clearfix content"> 
+	
+		<!-- content start　Q&A -->
+		<section id="contentBoxLarge">
+			<h1 class="contentLarge_h1">Q &amp; A</h1>
+			
+			<!-- 使用方法・設置場所等 -->
+			<h2 class="contentLarge_h2">使用方法・設置場所等</h2>
+			<div class="floatR mgb10"><p class="allOpenBtn"><span>全て開く</span> </p></div>
+			<div class="qList clearBoth">
+				<dl>
+					<dt id="q01_01"><span><strong>Q1.</strong> どのような方が対象となりますか。</span></dt>
+					<dd style="display:none"><span>独居や日中独居になる方々を想定しています。また、要介護の方々は当然ですが、要介護になる前の状態の方々も対象のイメージをしております。<br>
+例1） 要支援1、一人暮らしの高齢者。まだまだ元気で外にも出かけることもあるが、離れて暮らす息子さんが心配。<br>
+例2） 要介護1、二世帯住宅の1階に住んでいる。いつもエアコンをつけず熱中症になる可能性があるため心配。<br>
+例3） 要介護2、日中いつも寝てしまっている。夜間どの程度動いているのか把握したい。<br>
+例4） 要介護1、退院したが、体力低下が気になるため、日中の運動量を把握したい。<br>
+など<br>
+※多くのケースでの利用が想定されます。適応の有無に関する相談もお受けしております。</span></dd>
+					<dt id="q01_02"><span><strong>Q2.</strong> インターネット環境がないと利用できませんか。</span></dt>
+					<dd style="display:none"><span>インターネット環境がない場合にはポケットルーター（1500円/月）を貸し出すことで利用が可能となります。</span></dd>
+					<dt id="q01_03"><span><strong>Q3.</strong> CareEyeの大きさはどのくらいですか。</span></dt>
+					<dd style="display:none"><span>コンセントカバーくらいの大きさです。9.5㎝×10.8㎝（厚さ3.5㎝）</span></dd>
+					<dt id="q01_04"><span><strong>Q4. </strong> 取付は難しいですか。</span></dt>
+					<dd style="display:none"><span>難しくありません。基本的には設置位置を決めて取り付け、インターネット回線に接続、コンセントを差すだけです。</span></dd>
+					<dt id="q01_05"><span><strong>Q5. </strong> どこに付けるのが適切ですか。</span></dt>
+					<dd style="display:none"><span>日中の滞在時間が長い場所に設置することをお勧めいたします。（目的によっては寝室等に設置します。）<br>
+インターネットモデムと本体をケーブルでつなぐ必要があります。インターネットモデムの設置位置近くで、電源が確保できる壁に取り付けます。<br>
+（ビスもしくは両面テープを使用）</span></dd>
+				</dl>
+			</div>
+			<!-- /使用方法・設置場所等 --> 
+			
+			<!-- データの確認方法等 -->
+			<h2 class="contentLarge_h2">データの確認方法等</h2>
+			<div class="floatR mgb10"><p class="allOpenBtn"><span>全て開く</span> </p></div>
+			<div class="qList clearBoth">
+				<dl>
+					<dt id="q02_01"><span><strong>Q1.</strong> 見守りのデータはどのように見ることができますか。</span></dt>
+					<dd style="display:none"><span>パソコンやスマートフォン・タブレット端末など、インターネットを見ることが出来る環境があればどこでも見られます。<br>
+（閲覧にはインターネット通信料が発生します）<br>
+Apple社製端末（iPad、iPhone）用には専用のアプリも無料でダウンロードして頂けます。</span></dd>
+					<dt id="q02_02"><span><strong>Q2.</strong> Apple社製端末以外のスマートフォンでは見られませんか。</span></dt>
+					<dd style="display:none"><span>インターネットを見ることが出来ればどのスマートフォンでも見られます。（閲覧にはインターネット通信料が発生します）<br>
+ただしパンフレットに掲載されているスマートフォン用アプリは現状Apple社製端末（iPad、iPhone）のみの対応となります。<br>
+Android端末対応は2016年内を目処にご提供する予定です。</span></dd>
+					<dt id="q02_03"><span><strong>Q3.</strong> スマートフォンではない携帯電話では見られませんか。</span></dt>
+					<dd style="display:none"><span>インターネットを見ることが出来れば可能ですが、通信料が高額になる恐れがあります。<br>
+パソコンで見て頂く方法をおすすめします。</span></dd>
+					<dt id="q02_04"><span><strong>Q4. </strong> 異常時の警告・通知はどのように知らされますか。</span></dt>
+					<dd style="display:none"><span>室内が30度を超える高温など、異常時に通知する機能があります。<br>
+任意のメールアドレス宛に電子メールで通知する方法と、スマートフォン用アプリで画面上に通知する方法があります。（両方通知することも可能です）</span></dd>
+				</dl>
+			</div>
+			<!-- /データの確認方法等 --> 
+			
+			<!-- モニターキャンペーンについて -->
+			<h2 class="contentLarge_h2">モニターキャンペーンについて</h2>
+			<div class="floatR mgb10"><p class="allOpenBtn"><span>全て開く</span> </p></div>
+			<div class="qList clearBoth">
+				<dl>
+					<dt id="q03_01"><span><strong>Q1.</strong> 限定台数は何台ですか。</span></dt>
+					<dd style="display:none"><span>お客様向けは先着400台、社員向けは先着100台を予定しています。</span></dd>
+					<dt id="q03_02"><span><strong>Q2.</strong> 申込期限はありますか。</span></dt>
+					<dd style="display:none"><span></span>2016年4月～2016年12月31日までの予定です。サービス開始は5月初旬を予定しているため、それまでは予約受付となります。</dd>
+					<dt id="q03_03"><span><strong>Q3.</strong> キャンペーン終了後はどうなりますか。</span></dt>
+					<dd style="display:none"><span>キャンペ―ン終了後は、利用継続か中止かをお伺いいたします。<br>
+継続の場合は、そのまま機器をお使いいただき、月額使用料が発生いたします。この場合も初期費用は発生いたしません。<br>
+中止の場合は、機器を回収させて頂きます。特別な料金は発生いたしません。</span></dd>
+					<dt id="q03_04"><span><strong>Q4. </strong> 料金が発生した場合支払い方法はどうなりますか。</span></dt>
+					<dd style="display:none"><span>お支払いは全て、介護サービスの利用料と合算してご請求させて頂きます。</span></dd>
+					<dt id="q03_05"><span><strong>Q5. </strong> モニターキャンペーンの利用条件はありますか。</span></dt>
+					<dd style="display:none"><span>まごころ介護サービスをご利用のお客様のみご利用いただけます。<br>
+数回のアンケート調査にご協力をお願いする予定です。</span></dd>
+				</dl>
+			</div>
+			<!-- /モニターキャンペーンについて -->  
+			
+			<!-- 利用料・支払い方法について ※表示金額は全て税抜きです -->
+			<h2 class="contentLarge_h2">利用料・支払い方法について <span class="small">※表示金額は全て税抜きです</span></h2>
+			<div class="floatR mgb10"><p class="allOpenBtn"><span>全て開く</span> </p></div>
+			<div class="qList clearBoth">
+				<dl>
+					<dt id="q04_01"><span><strong>Q1.</strong> 利用料はいくらですか。</span></dt>
+					<dd style="display:none"><span><strong class="text_red large">初期費用14,800円、月額980円（年払い）</strong>となります。<br>
+月払い（月あたり1,200円）、6ヶ月払い（月あたり1,100円）もご用意しておりますが、年払いが一番お得です。<br>
+<br>
+【参考・支払金額）】<br>
+① 年払い 11,760円✕1回 （月あたり980円、年11,760円、<strong class="text_blue large">③より年2,640円お得</strong>）<br>
+② 6ヶ月払い 6,600円✕2回 （月あたり1,100円、年13,200円、<strong class="text_blue large">③より年1,200円お得</strong>）<br>
+③ 月払い 1,200円✕12回（月あたり1,200円、年14,400円）</span><br>
+ <span class="small">※表示金額は全て税抜きです</span></dd>
+					<dt id="q04_02"><span><strong>Q2.</strong> 料金が発生した場合支払い方法はどうなりますか。</span></dt>
+					<dd style="display:none"><span></span>お支払いは全て、介護サービスの利用料と合算してご請求させて頂きます。</dd>
+					<dt id="q04_03"><span><strong>Q3.</strong> 年払い・6か月払いの途中解約は返金がありますか。</span></dt>
+					<dd style="display:none"><span>途中解約での返金はございません。</span></dd>
+					<dt id="q04_04"><span><strong>Q4. </strong> ポケットルーター使用料も利用料と併せて年払いになりますか。</span></dt>
+					<dd style="display:none"><span>利用料とあわせての支払いになります。<br><br>
+【参考・支払金額）】<br>
+① 年払い 11,760円＋18,000円＝29,760円（月あたり2,480円）<br>
+② 6ヶ月払い 6,600円＋9,000円＝15,600円✕2回（月あたり2,600円、年31,200円）<br>
+③ 月払い 1,200円＋1,500円＝2,700円✕12回（月あたり2,700円、年32,400円）</span><br>
+ <span class="small">※表示金額は全て税抜きです</span></dd>
+				</dl>
+			</div>
+			<!-- /利用料・支払い方法について ※表示金額は全て税抜きです --> 
+		</section>
+		<!-- /content end　Q&A --> 
+		
+	</div>
+</main>
+<footer>
+	<ul>
+		<li><a href="http://careeye.jp/page/news">運営者からのお知らせ</a></li>
+		<li><a href="http://careeye.jp/page/terms">利用規約</a></li>
+		<li><a href="http://www.infic.net/" target="_blank">運営会社</a></li>
+		<li><a href="http://careeye.jp/page/privacy">プライバシーポリシー</a></li>
+		<li><a href="http://careeye.jp/page/help">Q&amp;A</a></li>
+		<li><a href="http://careeye.jp/contact">お問い合わせ</a></li>
+	</ul>
+	<p><span class="ftr_copyrights">&copy;</span> CareEye. All Rights Reserved.</p>
+</footer>
+<p id="page-top"><a href="#home"><img src="../images/common/btn_pagetop.png" width="59" height="59" alt=""/></a></p>
+<script src="js/content.js" type="text/javascript"></script>
+
+
+
+<!--このページのjs -->
+<script type="text/javascript" src="http://www.google.com/jsapi"></script> 
+<script type="text/javascript">google.load("jquery", "1.4");</script> 
+<script type="text/javascript" src="jquery.switchHat.js"></script> 
+<script>
+$(function(){
+	$.uHat.switchHat({
+		switchBtn: 'dl.faq dt',
+		switchContents: 'dl.faq dd'
+	});
+	//$.uHat.close();
+	$.uHat.openAll({
+		switchBtn: dl.faq dt,
+		openContents: 'dl.faq1 dd'
+		openContents: 'dl.faq2 dd'
+	});
+});
+</script> 
+<!-- /このページのjs -->
+</body>
+</html>
