d1b0f98c7c3a867b8fcb9b49025fa647151f50c5
Ikko Fukuda <fukuda@garoo.co.jp>
2016-06-03 04:59:10 +0900

    センサー通知保存のテスト

--- fuel/app/classes/controller/api/user/sensor.php
+++ fuel/app/classes/controller/api/user/sensor.php
@@ -32,8 +32,6 @@ class Controller_Api_User_Sensor extends Controller_Api
 
 	public function _save() {
 		$params = Input::param();
-		print_r($params);
-		exit;
 		$sensor = \Model_User_Sensor::saveUserSensor($params);
 		$this->result = array(
 			'data' => $sensor
