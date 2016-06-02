154a1edd2558d040b84c09d13698230a048fc1a8
Ikko Fukuda <fukuda@garoo.co.jp>
2016-06-03 05:23:29 +0900

    センサー通知保存のテスト

--- fuel/app/classes/model/user/sensor.php
+++ fuel/app/classes/model/user/sensor.php
@@ -76,19 +76,14 @@ class Model_User_Sensor extends Orm\Model{
 				$user_sensor = \Model_User_Sensor::forge();
 			}
 		}
-		    		print_r($params);
-
     	if($user_sensor) {
     		unset($params['q']);
     		unset($params['id']);
     		unset($params['user_id']);
     		unset($params['sensor_id']);
     		$user_sensor->set($params);
-    		if($user_sensor->save(false)) {
-    					print_r($params);
-		exit;
-    			return $user_sensor;
-    		}
+    		$user_sensor->save(false);
+    		return $user_sensor;
     	}
     	return null;
     }
