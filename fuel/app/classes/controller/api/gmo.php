cc7d49dece09949cb27b5f4a36d9d84e7fe50b0a
Ikko Fukuda <fukuda@garoo.co.jp>
2016-08-02 15:17:21 +0900

    結果通知受信の追加

--- fuel/app/classes/controller/api/gmo.php
+++ fuel/app/classes/controller/api/gmo.php
@@ -1,8 +1,15 @@
 <?php
 class Controller_Api_Gmo extends Controller_Api
 {
+	public function before() {
+		$this->nologin_methods = array(
+	        'result',
+	    );
+	    parent::before();
+	}
 
-	function get_index() {
+	function get_result() {
+		Log::info("GMO Result:".print_r(Input::post(), true));
 	}
 }
 ?>
