98abe1c318a493b401ecb335ced6663bcd44f152
Ikko Fukuda <fukuda@garoo.co.jp>
2016-08-09 15:08:36 +0900

    平均熱中症指数を追加

--- fuel/app/classes/controller/api/data.php
+++ fuel/app/classes/controller/api/data.php
@@ -121,6 +121,115 @@ class Controller_Api_Data extends Controller_Api
 		return $this->result();	
 	}
 
+	public function get_graph_org() {
+		return $this->_graph_org();
+	}
+
+	public function post_graph_org() {
+		return $this->_graph_org();
+	}
+
+	public function _graph_org() {
+		$type = Input::param("type");
+		$date = Input::param("date");
+		$span = Input::param("span");
+
+		$types = array(
+			'temperature' => true,					//温度
+			'humidity' => true,						//湿度
+			'illuminance' => true,					//照度
+			'active' => true,						//運動
+			'discomfort' => true,					//不快指数
+		);
+		$sensor_name = Input::param("sensor_name");
+		$sensor_id = Input::param("sensor_id");
+		if(empty($sensor_name) && empty($sensor_id)) {
+			$this->errors[] = array(
+				'message' => 'センサーIDを指定してください'
+			);
+		} else if(!empty($sensor_name)) {
+			$sensor = \Model_Sensor::getSensorFromSensorName($sensor_name);
+		} else if(!empty($sensor_id)) {
+			$sensor = \Model_Sensor::getSensor($sensor_id);
+		}
+
+		if(!$type) {
+			$this->errors[] = array(
+				'message' => 'グラフのタイプを指定してください'
+			);
+		} else if(empty($types[$type])) {
+			$this->errors[] = array(
+				'message' => 'グラフのタイプが間違っています'
+			);
+		} else if(!empty($sensor)) {
+			if(empty($date)) {
+				$date = date("Y-m-d");
+			} else {
+				$date = date("Y-m-d", strtotime($date));
+			}
+			if(empty($span)) {
+				$span = 10;
+			} else if($span < 0) {
+				$span = 1;
+			} else if($span > 240) {
+				$span = 240;
+			}
+			$data = array();
+			$start_time = strtotime($date." 00:00:00");
+			$end_time = strtotime($date." 24:00:00");
+			$end = 60 * 24 / $span;
+
+			$sql = 'SELECT * FROM data WHERE sensor_id=:sensor_name AND date BETWEEN :start_time AND :end_time';
+			//$sql = "SELECT SUBSTRING(CONVERT(NVARCHAR, date, 114),0,16) AS date, avg(active), avg(temperature) FROM data WHERE sensor_id = :sensor_name AND date BETWEEN :start_time AND :end_time GROUP BY SUBSTRING(CONVERT(NVARCHAR, date, 114),0,16)";
+			$query = DB::query($sql);
+			$query->parameters(array(
+				'sensor_name' => $sensor->name,
+				'start_time' => date("Y-m-d H:i:s", $start_time),
+				'end_time' => date("Y-m-d H:i:s", $end_time),
+			));
+			$results = $query->execute('data');
+			$rows = array();
+
+			foreach($results as $result) {
+				$result = Model_Data::format($result);
+				$rows[$result['date']] = $result;
+			}
+
+			for($i = 0; $i <= $end; $i++) {
+				$time = $start_time + $i * 60 * $span;
+				$current_time = date("Y-m-d H:i:s", $time); 
+
+				if(!empty($rows[$current_time])) {
+					$value = $rows[$current_time][$type];
+				} else {
+					$value = null;
+				}
+				
+				$data[] = array(
+					'time' => $current_time,
+					'label' => date("H:i", $time),
+					'value' => $value,
+					'temperature' => !empty($rows[$current_time]) ? $rows[$current_time]['temperature'] : null,
+					'humidity' => !empty($rows[$current_time]) ? $rows[$current_time]['humidity'] : null,
+					'illuminance' => !empty($rows[$current_time]) ? $rows[$current_time]['illuminance'] : null,
+					'active' => !empty($rows[$current_time]) ? $rows[$current_time]['active'] : null,
+					'discomfort' => !empty($rows[$current_time]) ? $rows[$current_time]['discomfort'] : null,
+					'wbgt' => !empty($rows[$current_time]) ? $rows[$current_time]['wbgt'] : null,
+				);
+			}
+			$this->result = array(
+				'sensor_id' => $sensor->id,
+				'sensor_name' => $sensor->name,
+				'type' => $type,
+				'date' => $date,
+				'span' => $span,
+				'data' => $data,
+			);
+		}
+		return $this->result();	
+	}
+
+
 	public function get_graph() {
 		return $this->_graph();
 	}
@@ -140,7 +249,6 @@ class Controller_Api_Data extends Controller_Api
 			'illuminance' => true,					//照度
 			'active' => true,						//運動
 			'discomfort' => true,					//不快指数
-			'wbgt' => true,
 		);
 		$sensor_name = Input::param("sensor_name");
 		$sensor_id = Input::param("sensor_id");
@@ -429,4 +537,101 @@ class Controller_Api_Data extends Controller_Api
 				$params['sleep_time_average'] = $hour.":".$minutes.":00";
 			}
 
-    		$sql = 'SELECT * FROM data WHERE sensor_i
+    		$sql = 'SELECT * FROM data WHERE sensor_id = :sensor_id AND date BETWEEN :start_date AND :end_date';
+	    	$query = DB::query($sql);
+			$query->parameters(array(
+				'sensor_id' => $sensor->name,
+				'start_date' => $start_date,
+				'end_date' => $end_date,
+			));
+			$result = $query->execute('data');
+			$rows = $result->as_array();
+			$count = count($rows);
+			if($count > 0) {
+				$temperature_total = 0;
+				$humidity_total = 0;
+				$active_total = 0;
+				$illuminance_total = 0;
+				foreach($rows as $row) {
+					$temperature_total += $row['temperature'];
+					$humidity_total += $row['humidity'];
+					$active_total += $row['active'];
+					$illuminance_total += $row['illuminance'];
+				}
+				$params['temperature_average'] = $temperature_total / $count;
+				$params['humidity_average'] = $humidity_total / $count;
+				$params['active_average'] = $active_total / $count;
+				$params['illuminance_average'] = $illuminance_total / $count;
+
+				$data_daily = \Model_Data_Daily::find('first', array('where' => array(
+					'sensor_id' => $sensor->id,
+					'date' => $date,
+				)));
+				if(empty($data_daily)) {
+					$data_daily =  \Model_Data_Daily::forge();
+				}
+				$data_daily->set($params);
+				$data_daily->save();	
+			}
+		} 
+		return $this->result();	
+	}
+
+	public function get_alert() {
+		$time = strtotime(date("Y-m-d H:i:00"));
+		//開通確認
+		$sensors = \Model_Sensor::find("all", array(
+			'where' => array(
+				'enable' => 0,
+				array('shipping_date', "<", date("Y-m-d H:i:s"))
+			)
+		));
+		foreach($sensors as $sensor) {
+			$result = DB::select("*")
+					    ->from('data')
+					    ->where('sensor_id', $sensor->name)
+					    ->where('date', date("Y-m-d H:i:00", $time - 60))
+					    ->execute('data');
+			if(isset($result)) {
+				$sensor->set(array('enable' => 1));
+				$sensor->save();
+			}
+		}
+
+
+		if(Input::param("sensor_id")) {
+			$sensors = array(\Model_Sensor::find(Input::param("sensor_id")));
+		} else {
+			$sensors = \Model_Sensor::find("all", array(
+				'where' => array(
+					'enable' => 1,
+				)
+			));
+		}
+		foreach($sensors as $sensor) {
+			$sensor->users;
+			$sensor->setTime($time);
+			try {
+				$this->result['data'][] = array(
+					'sensor_id' => $sensor->id,
+					'disconnection' => $sensor->checkDisconnection(),				//通信断アラート
+					'fire' => $sensor->checkFire(),									//火事アラート
+					'temperature' => $sensor->checkTemperature(),					//室温異常通知
+					'heatstroke' => $sensor->checkHeatstroke(),						//熱中症アラート
+					'humidity' => $sensor->checkHumidity(),							//室内湿度異常アラート
+					'mold_mites' => $sensor->checkMoldMites(),						//カビ・ダニ警報アラート
+					'illuminance_daytime' => $sensor->checkIlluminanceDaytime(),	//室内照度異常（日中）
+					'illuminance_night' => $sensor->checkIlluminanceNight(),		//室内照度異常（深夜）
+					'wake_up' => $sensor->checkWakeUp(),							//起床時間 //平均起床時間遅延
+					'sleep' => $sensor->checkSleep(),								//就寝時間 //平均睡眠時間遅延
+					'abnormal_behavior' => $sensor->checkAbnormalBehavior(),		//異常行動（夜間、照明をつけずに動いている）
+					'active_non_detection' => $sensor->checkActiveNonDetection(),	//一定時間人感センサー未感知
+																					//通信復帰通知
+            	);
+			} catch (Exception $e) {
+				Log::info($e->getMessage(), 'alert error');
+			}
+        }
+        return $this->result(); 
+    }
+}
