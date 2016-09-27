0a854b5e855129c3f98c98ad53feffd2f63a8259
Ikko Fukuda <fukuda@garoo.co.jp>
2016-09-27 18:09:00 +0900

    GMOの本番のconfigをcpする処理

--- deploy.cmd
+++ deploy.cmd
@@ -74,7 +74,9 @@ IF EXIST "%DEPLOYMENT_TARGET%\composer.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
-IF "%FUEL_ENV%" = "production" (
+IF "%FUEL_ENV%" NEQ "production" (
+  echo "%FUEL_ENV%"
+) else (
   call cp fuel/app/conf/connector.properties fuel/app/conf/connector.properties.develop
   call cp fuel/app/conf/connector.honban fuel/app/conf/connector.properties
 )
