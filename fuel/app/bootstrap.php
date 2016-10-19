bc637b867b7572868450ff1c0a293149dcd0fd8d
Ikko Fukuda <ikko615@gmail.com>
2016-10-19 16:39:28 +0900

    SimpleAuthの予約語エラープログラムを修正しGit内での管理に変更

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -21,6 +21,7 @@ require COREPATH.'bootstrap.php';
 	    'Database_Query_Builder_Select' => APPPATH.'classes/database/query/builder/select.php',
 	    'Database_Query_Builder_Delete' => APPPATH.'classes/database/query/builder/delete.php',
 	    'Database_Connection' => APPPATH.'classes/database/connection.php',
+	    'Auth_Login_Simpleauth' => APPPATH.'classes/auth/classes/auth/login/simpleauth.php',
 	));
 //}
 
