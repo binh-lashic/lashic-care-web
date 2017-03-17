607e970dff8b945ea29c7061693d18c25c19182d
Takeshi Nishino <bamblogs@Takeshi-no-MacBook.local>
2017-03-17 11:03:31 +0900

    INFIC-287 言語設定の警告をなくす

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -80,6 +80,8 @@ return array(
 	// 'language'           => 'en', // Default language
 	// 'language_fallback'  => 'en', // Fallback language when file isn't available for default language
 	// 'locale'             => 'en_US', // PHP set_locale() setting, null to not set
+	'language'           => 'ja',
+	'locale'             => 'jpn',
 
 	/**
 	 * Internal string encoding charset
