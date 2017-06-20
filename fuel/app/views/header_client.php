4175e59f58040056bee610e7d5fcf60e3d6bfa61
akishin <akishin999@gmail.com>
2017-06-20 11:52:50 +0900

    INFIC-402 アラート文言を多言語化設定ファイルから取得するよう変更

--- fuel/app/views/header_client.php
+++ fuel/app/views/header_client.php
@@ -108,8 +108,8 @@ if(!empty($this->data['header_alerts'])) {
 			<dl class="clearfix">
 				<dt class="com_news_tile"><img src="/images/common/hdr_news_megaphone.png" alt="icon"/><br>
 					<?php echo __('header.client.notice'); ?></dt>
-				<dd class="com_news_tile"> <a href="/user/report" class="com_news_link"> <span class="com_news_text"><?php echo $this->data['header_alerts'][0]['description']; ?><br>
-					<span class="small"><?php echo __('header.client.uncorresponding_items', ['count' => $this->data['header_alert_count']]); ?></span> </span> </a> </dd>
+				<dd class="com_news_tile"> <a href="/user/report" class="com_news_link"> <span class="com_news_text"><?php echo __("alerts.{$header_alerts[0]['type']}.description"); ?><br>
+					<span class="small"><?php echo __('header.client.uncorresponding_items', ['count' => $header_alert_count]); ?></span> </span> </a> </dd>
 			</dl>
 <?php
 }
