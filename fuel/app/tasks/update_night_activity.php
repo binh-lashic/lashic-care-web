d5be8450d905303f77b65bb54f48e4bd3d010efc
akishin <akishin999@gmail.com>
2017-07-12 23:10:31 +0900

    INFIC-439 night_acitivity更新スクリプトに再実行機能追加

--- fuel/app/tasks/update_night_activity.php
+++ fuel/app/tasks/update_night_activity.php
@@ -14,6 +14,10 @@ class Update_Night_Activity
 	/** タイムゾーン */
 	private $_timezone = null;
 
+	private $_current_sensor_log_path = null;
+	private $_current_sensor_name  = null;
+	private $_current_summary_date = null;
+
 	/**
 	 * daily report である sensordaily テーブルの night_activity を計算する
 	 * $ymd は Y-m-d 形式の文字列(ex. 2017-01-01)
@@ -22,6 +26,8 @@ class Update_Night_Activity
 	{
 		$this->_arg_ymd  = $ymd;
 		$this->_timezone = new \DateTimeZone('Asia/Tokyo');
+		$this->_current_sensor_log_path = implode(DIRECTORY_SEPARATOR, [rtrim(APPPATH, DIRECTORY_SEPARATOR), 'tmp', 'update_night_activity_current_sensor.txt']);
+		list($this->_current_sensor_name, $this->_current_summary_date) = $this->read_current_sensor();
 
 		# 集計の最大日時
 		if ($this->_arg_ymd) {
@@ -31,7 +37,7 @@ class Update_Night_Activity
 			$max_datetime = new \DateTimeImmutable('2017-07-03 00:00:01', $this->_timezone);
 		}
 
-		\Log::info("task [update_night_activity:run] start. max_datetime:[{$max_datetime->format('Y-m-d')}]", __METHOD__);
+		\Log::info("task [update_night_activity:run] start. max_datetime:[{$max_datetime->format('Y-m-d')}] current_sensor_name:[{$this->_current_sensor_name}] current_summary_date:[{$this->_current_summary_date}]", __METHOD__);
 
 		try {
 			// shipping_date が入っている通常センサーの一覧を取得
@@ -74,10 +80,29 @@ class Update_Night_Activity
 	private function update_night_activities(array $sensor_dailies)
 	{
 		foreach ($sensor_dailies as $sensor_daily) {
-			$summary_date = new \DateTimeImmutable($sensor_daily['summary_date']->format('Y-m-d H:i:s'), $this->_timezone);
-
 			$sensor_name = $sensor_daily['PartitionKey'];
-			$row_key     = $sensor_daily['RowKey'];
+			if ($this->_current_sensor_name) {
+				if ($this->_current_sensor_name !== $sensor_name) {
+					\Log::debug("sensor_name:[{$sensor_name}] current_sensor_name:[{$this->_current_sensor_name}] skipped.", __METHOD__);
+					continue;
+				} else {
+					$this->_current_sensor_name = null;
+				}
+			}
+
+			$summary_date_str = $sensor_daily['summary_date']->format('Y-m-d H:i:s');
+			if ($this->_current_summary_date) {
+				if ($this->_current_summary_date !== $summary_date_str) {
+					\Log::debug("sensor_name:[{$sensor_name}] summary_date:{$summary_date_str} current_summary_date:[{$this->_current_summary_date}] skipped.", __METHOD__);
+					continue;
+				} else {
+					$summary_date_str = $this->_current_summary_date;
+					$this->_current_summary_date = null;
+				}
+			}
+			$summary_date = new \DateTimeImmutable($summary_date_str, $this->_timezone);
+
+			$row_key = $sensor_daily['RowKey'];
 
 			# 前日 22:00 以降
 			$from = $summary_date->modify('-1 days')->setTime(22, 00, 00);
@@ -89,12 +114,15 @@ class Update_Night_Activity
 
 			\Log::debug("sensor_name:[{$sensor_name}] row_key:[{$row_key}] from:[{$from->format('Y-m-d H:i:s')}] to:[{$to->format('Y-m-d H:i:s')}] night_acitivity:[{$night_acitivity}]", __METHOD__);
 
-			echo "sensor_name:[{$sensor_name}] row_key:[{$row_key}] night_activity:[{$night_acitivity}]" . PHP_EOL;
-
 			$properties = [
 				'night_acitivity' => $night_acitivity,
 			];
 			$this->retryable_execute(['\Model_Sensordaily', 'merge_property'], [$sensor_name, $row_key, $properties]);
+
+			echo "sensor_name:[{$sensor_name}] row_key:[{$row_key}] night_activity:[{$night_acitivity}]" . PHP_EOL;
+
+			# 処理完了した sensor_name と日時をファイルに保存しておく
+			$this->write_current_sensor($sensor_name, $summary_date_str);
 		}
 	}
 
@@ -143,6 +171,31 @@ class Update_Night_Activity
 		$file    = $e->getFile();
 		$line    = $e->getLine();
 		\Log::warning("Error code:[{$code}] Error message:[{$message}] - file:[{$file}:{$line}]", __METHOD__);
+	}
+
+	/**
+	 * 現在処理中のセンサー情報をファイルに記録する
+	 */
+	private function write_current_sensor($sensor_name, $summary_date)
+	{
+		$message = "{$sensor_name},{$summary_date}";
+		$fp = fopen($this->_current_sensor_log_path, 'w');
+		if (flock($fp, LOCK_EX)) {
+			fputs($fp, $message);
+			flock($fp, LOCK_UN);
+		}
+		fclose($fp);
+	}
+
+	/**
+	 * 現在処理中のセンサー情報ファイルが存在する場合に内容を返す
+	 */
+	private function read_current_sensor()
+	{
+		if (file_exists($this->_current_sensor_log_path)) {
+			return explode(",", trim(file_get_contents($this->_current_sensor_log_path)));
+		}
+		return [null, null];
 	}
 }
 /* End of file tasks/create_sensor_monthly.php */
