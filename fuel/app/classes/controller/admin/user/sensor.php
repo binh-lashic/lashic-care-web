15e2ef35316d2d28a480308336355606b39f2239
fukuyo <yfd38073@nifty.com>
2017-10-16 16:14:17 +0900

    INFIC-529 親アカウントのセンサー割当解除を行うと見守られユーザーも削除される

--- fuel/app/classes/controller/admin/user/sensor.php
+++ fuel/app/classes/controller/admin/user/sensor.php
@@ -19,20 +19,20 @@ class Controller_Admin_User_Sensor extends Controller_Admin
     public function action_index()
     {
         $this->template->title = '会員ページ';
-        $this->template->content = Presenter::forge('admin/user/sensor');      
+        $this->template->content = Presenter::forge('admin/user/sensor/index');      
     }
 
+    /*
+     * センサー割当解除
+     * 
+     * @access public
+     */
     public function action_delete()
     {
-        $user_id = Input::param("user_id");
-        $sensor_id = Input::param("sensor_id");
-
-        \Model_User_Sensor::deleteUserSensor(array('user_id' => $user_id, 'sensor_id' => $sensor_id));
-
-        $client = \Model_Sensor::getClient(array('sensor_id' => $sensor_id));
-        if(isset($client)) {
-            \Model_User_Client::deleteUserClient(array('user_id' => $user_id, 'client_user_id' => $client->id));
-        }
-        Response::redirect('/admin/user/sensor?id='.$user_id); 
+        $this->template->content = Presenter::forge(
+                'admin/user/sensor/delete', 
+                'view', 
+                null, 
+                'admin/user/sensor/index');
     }
 }
