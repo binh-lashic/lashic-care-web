43ba6be628eab4df7f9452e7dedb76be2b5f393f
t-iwasaki <t-iwasaki@infic.net>
2019-12-19 17:09:46 +0900

    LASHIC-435 HTMLの差し替え

--- public/lp/js/main.js
+++ public/lp/js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-
     //plansテーブルのidの対応表
     const WIFI_RENTALS = {
         1:5,
@@ -7,15 +6,7 @@ $(function(){
         3:7
     }
 
-   function api(action, params, callback){
-      $.post(
-         "/api/" + action,
-         params,
-         callback,
-         "json"
-      );
-   }
-
+    scrollTop('.toPageTop', 500);
    $("input:button").click(function(event) {
       var plan_ids = [];
       var plan = $(this).data('plan');
@@ -30,3 +21,33 @@ $(function(){
       });
    });
 });
+
+/**
+ * toPageTop
+ */
+function scrollTop(elem, duration) {
+    let target = document.querySelector(elem);
+    target.addEventListener('click', function () {
+        let currentY = window.pageYOffset;
+        let step = duration / currentY > 1 ? 10 : 100;
+        let timeStep = duration / currentY * step;
+        let intervalID = setInterval(scrollUp, timeStep);
+        function scrollUp() {
+            currentY = window.pageYOffset;
+            if (currentY === 0) {
+                clearInterval(intervalID);
+            } else {
+                scrollBy(0, -step);
+            }
+        }
+    });
+}
+
+function api(action, params, callback){
+    $.post(
+        "/api/" + action,
+        params,
+        callback,
+        "json"
+    );
+}
