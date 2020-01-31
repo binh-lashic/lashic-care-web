a28e0d4ff95e0f0037fe4faf0e520ab25b2782fd
t-iwasaki <t-iwasaki@infic.net>
2020-01-31 14:05:09 +0900

    LASHIC-434 first_nameが漏れていたので追加

--- fuel/app/databases/ddl/20191218135000_LASHIC-434.sql
+++ fuel/app/databases/ddl/20191218135000_LASHIC-434.sql
@@ -6,4 +6,4 @@ ALTER TABLE [dbo].[payments] ALTER COLUMN user_id int null;
 ALTER TABLE [dbo].[contracts] ALTER COLUMN user_id int null;
 
 CREATE NONCLUSTERED INDEX idx_payments_token ON [dbo].[payments](token) INCLUDE (first_name, last_name, first_kana, last_kana, phone, email);
-CREATE NONCLUSTERED INDEX idx_payments_member_id ON [dbo].[payments](member_id) INCLUDE (last_name, first_kana, last_kana, phone, email);
+CREATE NONCLUSTERED INDEX idx_payments_member_id ON [dbo].[payments](member_id) INCLUDE (first_name, last_name, first_kana, last_kana, phone, email);
