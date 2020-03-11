b729b725ab99bb30de808c55080fabff1ed82fb1
Yutaro Mikami <lashic.002@gmail.com>
2020-03-11 11:30:10 +0900

    migファイル修正

--- fuel/app/databases/ddl/20200304141000_LASHIC-871.sql
+++ fuel/app/databases/ddl/20200304141000_LASHIC-871.sql
@@ -3,13 +3,18 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[agents](
+	[id] [int] IDENTITY(1,1) NOT NULL,
     [store_id] [int] NOT NULL,
-	[agent_code] [nvarchar] NOT NULL,
-	[agent_name] [nvarchar] NOT NULL
+	[agent_code] [nvarchar] (50) NOT NULL,
+	[agent_name] [nvarchar] (50) NOT NULL,
+	[created_at] [datetime] NULL,
+	[updated_at] [datetime] NULL
 )
 GO
 CREATE TABLE [dbo].[stores](
     [id] [int] IDENTITY(1,1) NOT NULL,
-	[store_name] [nvarchar] NOT NULL
+	[store_name] [nvarchar] (50)  NOT NULL,
+	[created_at] [datetime] NULL,
+	[updated_at] [datetime] NULL
 )
 GO
